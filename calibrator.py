f3bbff1cde8818d0daa0b14b42e4844a9daa8e61
montybytes <muturimonty@gmail.com>
2024-08-02 15:01:44 +0100

    cleaned up calibration script, added cli

--- calibrator.py
+++ calibrator.py
@@ -1,22 +1,15 @@
 import cv2
 import json
 import numpy as np
+import argparse
+
+from os import listdir
+from os.path import isfile, join
 
 import lib.vision as vision
 import lib.utils as utils
 
-
-# ? plans:
-# [-] check if blurring & denoising help edge detection
-# [-] use erosion and dilation to get vertical features
-# [-] use greyscale image
-# [-] try to use skeletonizing on vertical features
-# [-] weed out non-vertical detected lines and boundary cases e.g broadband signals
-# [ ] use a center line biasing method to assign confidence values to detected lines
-# [-] develop a pipleine to perform all steps on single image
-# [ ] evaluate performance using fact.py | create evaluation script
-
-title = "Simple Meteor Detection"
+title = "Simple Meteor Detection: Calibrator"
 
 img_index = "Image Index:"
 binary_thresh = "Bin Thresh:"
@@ -26,15 +19,42 @@ line_length = "Hgh Len:"
 line_gap = "Hgh Gap:"
 bb_thresh = "BB Thresh:"
 
-imgs = []
+img_path = None
+img_paths = []
 
-lines = []
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "file",
+    nargs="?",
+    default=None,
+    help="The image file to display during calibration",
+)
+parser.add_argument(
+    "--dir",
+    default=None,
+    nargs="?",
+    help="The folder to load calibration images from",
+)
+parser.add_argument(
+    "-s",
+    "--save",
+    default=True,
+    action="store_true",
+    help="Save config to file",
+)
+
+args = parser.parse_args()
+
+save_mode = args.save
 
 
-def get_img(window):
-    # change image undergoing processing to visualize the parameters on different images
-    index = cv2.getTrackbarPos(img_index, window)
-    return imgs[index]
+def get_image():
+    if len(img_paths) > 0:
+        index = cv2.getTrackbarPos(img_index, title)
+        return vision.load_image(img_paths[index], cv2.IMREAD_GRAYSCALE)
+    else:
+        return vision.load_image(img_path, cv2.IMREAD_GRAYSCALE)
 
 
 def detect_broadbands(bboxes):
@@ -66,11 +86,7 @@ def detect_broadbands(bboxes):
 
 
 def vertical_extraction(val=0):
-    # start_time = timeit.default_timer()
-
-    global title, imgs
-
-    img = get_img(title)
+    img = get_image()
 
     # *pass: removing legends and axes (unimportant areas)
     mask = vision.create_rectangle_mask(img, 64, 64, 607, 927)
@@ -113,55 +129,38 @@ def vertical_extraction(val=0):
     img_w, img_h = (927 - 64, 607 - 64)
     img_center_y = (img_h // 2) + 64
 
-    cv2.line(boxes_img, (64, img_center_y), (img_w + 64, img_center_y), (0, 0, 255), 2)
-
-    # !!
     broadbands = utils.flatten(detect_broadbands(bboxes))
 
     detections = [bbox for bbox in bboxes if bbox not in broadbands]
 
     for box in detections:
-        # calculate midpoints
         mid_x, mid_y = box.midpoint
         deviation = abs(mid_y - img_center_y)
         deviation = utils.mapFromTo(deviation, 0, img_h / 2, 0, 1)
         vision.draw_bbox(boxes_img, box, (0, 255 - 255 * deviation, 255 * deviation))
-
-    # final_detections = vision.draw_bboxes(img, detections)
-
-    # extra addon: make script save bounding boxes to file and also draw boxes on original image and save to folder
-
-    # end_time = timeit.default_timer() - start_time
-    # print("Executed in: ", end_time * 1000, "ms")
 
     cv2.imshow(title, boxes_img)
     return
 
 
 def main():
-    # -- images to test with --
-    img_paths = [
-        "dataset/images/train/BEBILZ_SYS001-2024-05-25-03-00.png",  # (only simple + complex meteors)
-        "dataset/images/train/BEBILZ_SYS001-2024-05-25-06-00.png",  # (many planes)
-        "dataset/images/train/BEBILZ_SYS001-2024-05-25-12-00.png",  # (many planes + complex meteor)
-        "dataset/images/train/BEBOEC_SYS001-2024-05-25-01-00.png",  # (very noisy)
-        "dataset/images/train/BEDINA_SYS001-2024-05-25-13-00.png",  # (no noise)
-        "dataset/images/test/BEBILZ_SYS001-2024-05-26-14-00.png",  # (many broadband signals)
-    ]
+    global img_path, img_paths
 
-    # loading all the images
-    for path in img_paths:
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    cv2.namedWindow(title)
 
-        if img is None:
-            print("Could not open or find the image: ", path)
+    if args.dir is not None:
+        files = listdir(args.dir)
+
+        if len(files) == 0:
+            print("No files found in given directory")
             exit(0)
 
-        imgs.append(img)
+        img_paths = [join(args.dir, f) for f in files if isfile(join(args.dir, f))]
+        cv2.createTrackbar(img_index, title, 0, len(img_paths) - 1, vertical_extraction)
 
-    # creating configuration ui
-    cv2.namedWindow(title)
-    cv2.createTrackbar(img_index, title, 0, len(imgs) - 1, vertical_extraction)
+    else:
+        img_path = args.file
+
     cv2.createTrackbar(binary_thresh, title, 14, 255, vertical_extraction)
     cv2.createTrackbar(row_count, title, 12, 32, vertical_extraction)
     cv2.createTrackbar(hough_thresh, title, 20, 100, vertical_extraction)
@@ -176,19 +175,20 @@ def main():
 
     cv2.waitKey()
 
-    # program parameters
-    data = {
-        "binary_threshold": cv2.getTrackbarPos(binary_thresh, title),
-        "row_count": cv2.getTrackbarPos(row_count, title),
-        "hough_threshold": cv2.getTrackbarPos(hough_thresh, title),
-        "hough_line_length": cv2.getTrackbarPos(line_length, title),
-        "hough_line_gap": cv2.getTrackbarPos(line_gap, title),
-        "broadband_thresh": cv2.getTrackbarPos(bb_thresh, title),
-    }
-
-    # save parameters to json file
-    with open("config.json", "w") as cfg_file:
-        json.dump(data, cfg_file, indent=2)
+    if save_mode:
+        # save parameters to json file
+        with open("config.json", "w") as cfg_file:
+            # program parameters
+            data = {
+                "binary_threshold": cv2.getTrackbarPos(binary_thresh, title),
+                "row_count": cv2.getTrackbarPos(row_count, title),
+                "hough_threshold": cv2.getTrackbarPos(hough_thresh, title),
+                "hough_line_length": cv2.getTrackbarPos(line_length, title),
+                "hough_line_gap": cv2.getTrackbarPos(line_gap, title),
+                "broadband_thresh": cv2.getTrackbarPos(bb_thresh, title),
+            }
+            json.dump(data, cfg_file, indent=2)
+            print("--\nconfiguration saved to file\n--")
 
     return
 
